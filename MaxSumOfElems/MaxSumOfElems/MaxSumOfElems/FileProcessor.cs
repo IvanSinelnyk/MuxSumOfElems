7c3476d7c0111b01460a65f49fb197f35490753f
Ivan_Sinelnyk <ivan7sinelnyk@gmail.com>
2023-05-25 09:28:05 +0200

    project added.

---
+++ MaxSumOfElems/MaxSumOfElems/MaxSumOfElems/FileProcessor.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxSumOfElems
+{
+    internal class FileProcessor
+    {
+        private readonly string path;
+
+        public FileProcessor(string? path)
+        {
+            if (path == null)
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+            this.path = path;
+        }
+
+        public int LineNumberWithMaxSumOfElems()
+        {
+            int numberMaxSumLine = 0;
+            decimal maxSum = 0m;
+            using (FileStream fileStream = new(path, FileMode.Open, FileAccess.Read))
+            using (StreamReader reader = new(fileStream))
+            {
+                int currentLine = 0;
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    ++currentLine;
+                    decimal tempMaxSum = 0m;
+                    string[] numbers = line.Split(',');
+                    foreach (string number in numbers)
+                    {
+                        if (decimal.TryParse(number, NumberStyles.Number, CultureInfo.InvariantCulture, out decimal temp))
+                        {
+                            tempMaxSum += temp;
+                        }
+                        else
+                        {
+                            tempMaxSum = 0m;
+                            break;
+                        }
+                    }
+                    if (tempMaxSum > maxSum)
+                    {
+                        maxSum = tempMaxSum;
+                        numberMaxSumLine = currentLine;
+                    }
+                }
+            }
+            return numberMaxSumLine;
+        }
+
+        public int[] GetListOfBrokenLines()
+        {
+            var list = new List<int>();
+            using (FileStream fileStream = new(path, FileMode.Open, FileAccess.Read))
+            using (StreamReader reader = new(fileStream))
+            {
+                int currentLine = 0;
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    ++currentLine;
+                    string[] numbers = line.Split(',');
+                    foreach (string number in numbers)
+                    {
+                        if (!decimal.TryParse(number, NumberStyles.Number, CultureInfo.InvariantCulture, out decimal _))
+                        {
+                            list.Add(currentLine);
+                        }
+                    }
+                }
+            }
+            var array = list.ToArray();
+            return array;
+        }
+
+        public void GetTxtFileWithBrokenLines()
+        {
+            using (StreamWriter writer = new(@"..\..\..\..\BrokenLinesList.txt"))
+            {
+                int[] result = GetListOfBrokenLines();
+                foreach (int number in result)
+                {
+                    writer.Write(number + " ");
+                }
+            }
+        }
+    }
+}
---
+++ MaxSumOfElems/MaxSumOfElems/MaxSumOfElems/Program.cs
@@ -0,0 +1,32 @@
+﻿using MaxSumOfElems;
+using System.Globalization;
+
+Console.WriteLine("Enter full path to file:");
+string? path = Console.ReadLine();
+//path = @"TestData.txt"; // Uncomment this line for testing from TestData.txt.
+try
+{
+    FileProcessor fp = new(path);
+    int lineNumberWithMaxSum = fp.LineNumberWithMaxSumOfElems();
+    Console.WriteLine("Number of line with maximum sum of numeric elements:");
+    Console.WriteLine(lineNumberWithMaxSum);
+    int[] brokenLines = fp.GetListOfBrokenLines();
+    Console.WriteLine("Numbers of lines with non numeric elements:");
+    foreach (int num in brokenLines)
+    {
+        Console.Write(num + " ");
+    }
+    fp.GetTxtFileWithBrokenLines();
+    Console.WriteLine();
+    Console.WriteLine("You can find the list of \"broken\" lines in this file:");
+    Console.WriteLine("BrokenLinesList.txt");
+}
+catch (ArgumentException)
+{
+    Console.WriteLine("No path was enter.");
+}
+catch (FileNotFoundException)
+{
+    Console.WriteLine("Wrong path was enter.");
+}
+
